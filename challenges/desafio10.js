b3de444613385de3bc37cc19a50bcafe927bb489
Kevillin Santos <kevillin_santos@hotmail.com>
2023-02-07 16:08:59 -0300

    Resolvido pendencias com lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,9 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { percentual: { $gte: 30, $lte: 40 }, tipo: 'proteínas' } } }, { nome: 1, _id: 0 });
+db.produtos.find({
+  valoresNutricionais:
+  {
+    $elemMatch: {
+      percentual:
+        { $gte: 30, $lte: 40 }, tipo: "proteínas"
+    }
+  }
+}, { nome: 1, _id: 0 });
