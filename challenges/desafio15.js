4f21b195442a8159e9521023ba7324e5d2585288
Kevillin Santos <kevillin_santos@hotmail.com>
2023-02-07 16:52:38 -0300

    Resolve pendencias com lint

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -4,15 +4,13 @@ db.produtos.updateMany(
 );
 db.produtos.updateMany(
   { tags: { $in: ["bovino"] } },
-  { $inc: { avaliacao: 5 } }
+  { $inc: { avaliacao: 5 } },
 );
 db.produtos.updateMany(
   { tags: { $in: ["ave"] } },
-  { $inc: { avaliacao: 3 } }
+  { $inc: { avaliacao: 3 } },
 );
 db.produtos.find(
   {},
-  { nome: 1, avaliacao: 1, _id: 0}
+  { nome: 1, avaliacao: 1, _id: 0 },
 );
-
-
--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -10,4 +10,3 @@ db.produtos.find(
   { ultimaModificacao: { $exists: true } },
   { nome: 1, _id: 0 },
 );
-
