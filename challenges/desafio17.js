c982633d4f6ce583e62d9de9ef739672542473f9
Kevillin Santos <kevillin_santos@hotmail.com>
2023-02-07 17:27:20 -0300

    Conserta pendencias Lint

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -2,4 +2,4 @@ db.resumoProdutos.insert({
   franquia: "McDonalds",
   totalProdutos: db.produtos.count(),
 });
-db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id:0 });
+db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
