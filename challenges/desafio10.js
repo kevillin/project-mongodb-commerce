62871d4db36d432ef5cbb52f72955dc09ec7b77e
Kevillin Santos <kevillin_santos@hotmail.com>
2023-02-07 16:12:15 -0300

    Resolve pendencias 2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,8 +2,8 @@ db.produtos.find({
   valoresNutricionais:
   {
     $elemMatch: {
-      percentual:
-        { $gte: 30, $lte: 40 }, tipo: "proteínas"
-    }
-  }
+      percentual: { $gte: 30, $lte: 40 },
+        tipo: "proteínas",
+    },
+  },
 }, { nome: 1, _id: 0 });
